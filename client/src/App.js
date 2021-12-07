57b73afde787890009cccecbaf229b784bdb4a54
lohszeying <bloom9334@hotmail.com>
2021-12-07 18:13:09 +0800

    Navbar and buttons

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 class App extends Component {
     constructor(props) {
@@ -21,13 +22,12 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
-            </div>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' />
+                </Routes>
+            </Router>
         );
     }
 }
