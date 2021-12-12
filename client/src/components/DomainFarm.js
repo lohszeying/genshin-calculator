c3ac587a1f78dd596959f0e1190d048176a654b3
lohszeying <bloom9334@hotmail.com>
2021-12-13 00:36:40 +0800

    edited table label for domain talent farm availability

--- client/src/components/DomainFarm.js
+++ client/src/components/DomainFarm.js
@@ -75,19 +75,19 @@ function DomainFarm({
 
                                 <table className="characters">
                                     <tr>
-                                        <td className="domainfarm__label">{typeOfMaterialLabel.one[0]}</td>
+                                        <td className="domainfarm__label">{typeOfMaterialLabel.two[0]}</td>
                                         <td className="domainfarm__character-img-wrapper">
                                             { (chars.two[0] || []).map(({src}) => <img src={src} key={src} alt={alt} className="domainfarm__characters-img" />) }
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td className="domainfarm__label">{typeOfMaterialLabel.one[1]}</td>
+                                        <td className="domainfarm__label">{typeOfMaterialLabel.two[1]}</td>
                                         <td className="domainfarm__character-img-wrapper">
                                             { (chars.two[1] || []).map(({src}) => <img src={src} key={src} alt={alt} className="domainfarm__characters-img" />) }
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td className="domainfarm__label">{typeOfMaterialLabel.one[2]}</td>
+                                        <td className="domainfarm__label">{typeOfMaterialLabel.two[2]}</td>
                                         <td className="domainfarm__character-img-wrapper">
                                             { (chars.two[2] || []).map(({src}) => <img src={src} key={src} alt={alt} className="domainfarm__characters-img" />) }
                                         </td>
@@ -113,19 +113,19 @@ function DomainFarm({
 
                                 <table className="characters">
                                     <tr>
-                                        <td className="domainfarm__label">{typeOfMaterialLabel.one[0]}</td>
+                                        <td className="domainfarm__label">{typeOfMaterialLabel.three[0]}</td>
                                         <td className="domainfarm__character-img-wrapper">
                                             { (chars.three[0] || []).map(({src}) => <img src={src} key={src} alt={alt} className="domainfarm__characters-img" />) }
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td className="domainfarm__label">{typeOfMaterialLabel.one[1]}</td>
+                                        <td className="domainfarm__label">{typeOfMaterialLabel.three[1]}</td>
                                         <td className="domainfarm__character-img-wrapper">
                                             { (chars.three[1] || []).map(({src}) => <img src={src} key={src} alt={alt} className="domainfarm__characters-img" />) }
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td className="domainfarm__label">{typeOfMaterialLabel.one[2]}</td>
+                                        <td className="domainfarm__label">{typeOfMaterialLabel.three[2]}</td>
                                         <td className="domainfarm__character-img-wrapper">
                                             { (chars.three[2] || []).map(({src}) => <img src={src} key={src} alt={alt} className="domainfarm__characters-img" />) }
                                         </td>
