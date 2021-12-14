a1cf7236fcafb09cd2f4e4bc434f2fb64d403288
lohszeying <bloom9334@hotmail.com>
2021-12-14 23:48:52 +0800

    updated footer, navbar and colour scheme

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -62,11 +62,6 @@ function Navbar() {
                                 Resin Material Calculator
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>
                         
                         {/*<li className="nav-btn">
                             {button ? (
