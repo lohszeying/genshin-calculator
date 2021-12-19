939e16f10267e496f16fb326d75f49da2586bd4e
lohszeying <bloom9334@hotmail.com>
2021-12-19 21:44:55 +0800

    added indexRouter

--- api/app.js
+++ api/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testAPI', testAPIRouter);
 app.use('/charExp', sendCharExpRouter);
