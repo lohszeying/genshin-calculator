8c3e95870438e147feeca302ae39999a6796e9d5
Loh Sze Ying <bloom9334@hotmail.com>
2023-01-15 18:48:39 +0800

    tried removing jade view

--- api/app.js
+++ api/app.js
@@ -14,8 +14,8 @@ var resinCalculatorRouter = require('./routes/ResinMaterialCalculator/resinmater
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(cors());
 app.options('*', cors());
